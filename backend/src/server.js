aeba79034d77e52cf033ce13e7a3f4a7af53617f
brenodev <breno.souza.developer@gmail.com>
2019-10-06 19:39:09 -0300

    criado o frotend e modificado o backend

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
+const path = require("path");
+
 const routes = require("./routes");
 
 const app = express();
@@ -15,7 +18,9 @@ mongoose.connect(
 // req.params = Acessar route params (para edição e delete)
 // req.body = Acessar corpo da requisição ( para criação, edição de registros)
 
+app.use(cors());
 app.use(express.json());
+app.use("/files", express.static(path.resolve(__dirname, "..", "uploads")));
 app.use(routes);
 
 app.listen(3333);
